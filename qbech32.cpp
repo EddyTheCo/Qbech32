952f2957555108bcee90ec90ee3c50bce6922a2d
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-05-14 17:19:15 +0200

    Develop (#17)
    * install dlls on bin dir (#14)
    
    * use latest commons, and added format action (#15)
    
    * C/C++/QML formatted code (#16)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- qbech32.cpp
+++ qbech32.cpp
@@ -1,38 +1,43 @@
-#include"encoding/qbech32.hpp"
-#include"segwit_addr.h"
-#include"iota_addr.h"
+#include "encoding/qbech32.hpp"
+#include "iota_addr.h"
+#include "segwit_addr.h"
 #include <QStringList>
-namespace qencoding{
-	namespace qbech32 {
-		namespace SegWit{
-			QString encode(const QString& hrp, const QByteArray& data, const quint32 witver )
-			{
-				auto result=segwit_addr::encode(hrp.toStdString(),witver,std::vector<uint8_t> (data.begin(), data.end()));
-				return QString::fromStdString(result);
-			}
-			std::pair<QString,QByteArray> decode(const QString& bstring)
-			{
-
-				auto list=bstring.split("1");
-
-				auto var=segwit_addr::decode(list[0].toStdString(),bstring.toStdString());
-
-				return std::make_pair(list[0], QByteArray(reinterpret_cast<const char*>(var.second.data()), var.second.size()));
-			}
-		}
-
-		namespace Iota{
-			QString encode(const QString& hrp, const QByteArray &data){
-				auto result=iota_addr::encode(hrp.toStdString(),std::vector<uint8_t> (data.begin(), data.end()));
-				return QString::fromStdString(result);
-
-			}
-			std::pair<QString,QByteArray> decode(const QString& bstring){
-				auto list=bstring.split("1");
-				auto var=iota_addr::decode(bstring.toStdString());
-
-				return std::make_pair(list[0], QByteArray(reinterpret_cast<const char*>(var.data()), var.size()));
-			}
-		}
-	}
+namespace qencoding
+{
+namespace qbech32
+{
+namespace SegWit
+{
+QString encode(const QString &hrp, const QByteArray &data, const quint32 witver)
+{
+    auto result = segwit_addr::encode(hrp.toStdString(), witver, std::vector<uint8_t>(data.begin(), data.end()));
+    return QString::fromStdString(result);
 }
+std::pair<QString, QByteArray> decode(const QString &bstring)
+{
+
+    auto list = bstring.split("1");
+
+    auto var = segwit_addr::decode(list[0].toStdString(), bstring.toStdString());
+
+    return std::make_pair(list[0], QByteArray(reinterpret_cast<const char *>(var.second.data()), var.second.size()));
+}
+} // namespace SegWit
+
+namespace Iota
+{
+QString encode(const QString &hrp, const QByteArray &data)
+{
+    auto result = iota_addr::encode(hrp.toStdString(), std::vector<uint8_t>(data.begin(), data.end()));
+    return QString::fromStdString(result);
+}
+std::pair<QString, QByteArray> decode(const QString &bstring)
+{
+    auto list = bstring.split("1");
+    auto var = iota_addr::decode(bstring.toStdString());
+
+    return std::make_pair(list[0], QByteArray(reinterpret_cast<const char *>(var.data()), var.size()));
+}
+} // namespace Iota
+} // namespace qbech32
+} // namespace qencoding
